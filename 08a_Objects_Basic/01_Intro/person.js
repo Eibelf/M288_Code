19520b91de2960919499653e52f2e929699f2313
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2020-10-27 00:19:55 +0100

    Update datatypes in JSBC Med 19

--- 08a_Objects_Basic/01_Intro/person.js
+++ 08a_Objects_Basic/01_Intro/person.js
@@ -1,36 +1,63 @@
 /*
-Eine Objekt-Vorlage wird in JavaScript mit class erstellt (nicht zu verwechseln CSS-Klasse)
+Eine Objekt-Vorlage wird in JavaScript mit class erstellt
+(nicht zu verwechseln CSS-Klasse)
+
 Beispiel für Person
  */
 
 /* Klasse */
 class Person{
+    //Property
+    firstName;
+    lastName;
+    gender;
+    birthday;
+    profession = "No profession set!";
+
     /* Konstruktor - erstellt das Objekt (sog. Instanz) */
-    constructor(firstName, lastName, geschlect, birthday) {
+    constructor(firstName, lastName, gender, birthday) {
         //Properties - Eigenschaften/Attribute einer Klasse
         this.firstName = firstName;
         this.lastName = lastName;
-        this.geschlecht = geschlecht;
+        this.gender = gender;
         this.birthday = birthday;
     }
 
     /* Methode (ähnlich wie Funktion) - kann Parameter haben */
     getFullName(){
-        return `Mein Name ist ${this.firstName} ${this.lastName}.`
+        return `${this.firstName}, ${this.lastName}.`
     }
 
     /* Getter - OHNE Parameter */
-    get fullName(){
-        return `Der Name ist ${this.firstName} ${this.lastName}.`
+    get gender(){
+        return `${this.gender}`;
+    }
+
+    get profession(){
+        return `${this.profession}`;
     }
 
     /* Setter ohne return */
-    set fullName(fullName) {
-        const names = fullName.split(' ')
-        this.firstName = names[0]
-        this.lastName = names[1]
+    set profession(prof) {
+        this.profession = prof;
     }
-
 }
 
 export default Person;
+
+
+/*
+Beispiel 1: Objekt person1 mit Klasse Person erzeugen
+Erzeugung des Objekts mit Schlüsselwort 'new' und dem Klassen-Namen.
+ */
+/*
+let person1 = new Person("Hans", "Muster", "männlich", "1973-11-30");
+//Ausgabe
+console.log(`Vorname, Nachname: ${person1.getFullName()}`);
+//Getter
+console.log(`Beruf: ${person1.profession}`);
+//Setter
+person1.profession = "Mediamatiker";
+//Getter
+console.log(`Beruf: ${person1.profession}`);
+*/
