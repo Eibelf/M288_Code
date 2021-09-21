092d7025c8d453d6c6243c3592161e4b5e4d1f2e
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2021-09-21 11:45:36 +0200

    Add little example for looping numbers from 1 to 100

--- 05a_Loops_Basic/01_Intro/02_loops.js
+++ 05a_Loops_Basic/01_Intro/02_loops.js
@@ -53,6 +53,17 @@ for(let i = 0; i < 4; i++) {
     }
 }
 
+//Zahlen von 1 bis 100 ausgeben
+for(let i=0;i<=100;i++){
+    console.log(i);
+}
+
+//Zahlen von 1 bis 100 addieren
+let sum =0;
+for(let i=0;i<=1000;i++){
+    sum = sum + i;
+}
+console.log(sum);
 
 
 
